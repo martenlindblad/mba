c7e090c9f4119b93230d2d9f1d2144754626f431
martenlindblad <martenlindblad@users.noreply.github.com>
2016-05-09 09:34:02 +0200

    Update startit.sh

--- startit.sh
+++ startit.sh
@@ -10,4 +10,4 @@ apt-get install docker-engine
 service docker start
 mkdir mba
 curl https://raw.githubusercontent.com/martenlindblad/mba/master/Scala%20-%20Associative%20Analysis.ipynb -o mba/MBA.ipynb
-docker run -d -p 8888:8888 -v mba:/home/jovyan/work jupyter/all-spark-notebook
+docker run -d -p 8888:8888 -v $HOME/mba:/home/jovyan/work jupyter/all-spark-notebook
