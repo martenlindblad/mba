78a266db0c3ac13ea03d2aff6fc3d15088502f28
martenlindblad <martenlindblad@users.noreply.github.com>
2016-05-09 14:16:17 +0200

    Update fixit.sh

--- fixit.sh
+++ fixit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm $HOME/MBA/Products/_SUCCESS
 rm $HOME/MBA/Rules/_SUCCESS
-cat $HOME/mba/MBA/Rules/* | perl -pe 's/"Source"://; s/^"//; s/",$//; s/{//g; s/}//g; s/"Target"://; s/"Support"://; s/"Confidence"://; s/"Lift"://; s/{//; s/}//' > Rules.csv
-cat $HOME/mba/MBA/Products/* | perl -pe 's/"Product"://; s/^"//; s/",$//; s/{//g; s/}//g; s/"Support"://' > Products.csv
+cat $HOME/MBA/Rules/* | perl -pe 's/"Source"://; s/^"//; s/",$//; s/{//g; s/}//g; s/"Target"://; s/"Support"://; s/"Confidence"://; s/"Lift"://; s/{//; s/}//' > Rules.csv
+cat $HOME/MBA/Products/* | perl -pe 's/"Product"://; s/^"//; s/",$//; s/{//g; s/}//g; s/"Support"://' > Products.csv
